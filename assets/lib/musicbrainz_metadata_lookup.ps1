e3710fe8f540591f97504d25f9287f651ce22a5b
stevehammoud <dybdkdzzrg@privaterelay.appleid.com>
2026-08-15 13:29:37 -0400

    Repair ASAP Rocky lookup patch

--- assets/lib/musicbrainz_metadata_lookup.ps1
+++ assets/lib/musicbrainz_metadata_lookup.ps1
@@ -65,6 +65,7 @@ function Get-FoldedText {
     $text = $text -replace '\bac\s*/?\s*dc\b', 'acdc'
     $text = $text -replace '\b(ft|feat|featuring|with|and|y|con|x)\b', ' '
     $text = $text -replace '[^\p{L}\p{Nd}]+', ' '
+    $text = $text -replace '\ba\s+ap\b', 'asap'
     return (($text -replace '\s+', ' ').Trim())
 }
 
